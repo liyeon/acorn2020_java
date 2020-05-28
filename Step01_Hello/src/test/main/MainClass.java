0c90e52ded8cc5c7afb92d309c73000c229e2067
liyeon <fldus612@gmail.com>
2020-05-28 17:04:17 +0900

    Step01_Hello/ Java파일 수정 /study

--- Step01_Hello/src/test/main/MainClass.java
+++ Step01_Hello/src/test/main/MainClass.java
@@ -1,7 +1,8 @@
 package test.main;
 
 public class MainClass {
-	// run(실행) 했을때 실행의 흐름이 시작되는 main 메소드 
+//클래스의 범위.
+	//run(실행) 했을 때 실행의 흐름이 시작되는 main메소드
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 	}
--- Step01_Hello/src/test/main/MainClass2.java
+++ Step01_Hello/src/test/main/MainClass2.java
@@ -1,11 +1,17 @@
 package test.main;
 
 public class MainClass2 {
-	//run 했을때 실행순서가 시작되는 특별한 main 메소드 
+	//run했을 때 실행순서가 실행되는 특별한 main메소드
 	public static void main(String[] args) {
-		System.out.println("main 메소드가 시작 되었습니다.");
+		System.out.println("main메소드가 시작되었습니다.");
 		System.out.println("하나");
 		System.out.println("두울");
-		System.out.println("main 메소드가 리턴(종료) 됩니다.");
+		System.out.println("main메소드가 리턴(종료)됩니다.");
+	//console.log와 똑같음
+		/*
+		 *기본 오브젝트 기본 함수 없다.
+		 *도구가 클래스 단위로 제공이 된다.
+		 *이클립스에서의 디버깅
+		 **/
 	}
 }
