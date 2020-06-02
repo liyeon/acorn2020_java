861425e8806a264539b70bc1de46225b9f222a45
liyeon <fldus612@gmail.com>
2020-06-02 18:21:10 +0900

    로또,문자열 생성 배열

--- Step08_array/src/test/main/MainClass06.java
+++ Step08_array/src/test/main/MainClass06.java
@@ -12,29 +12,28 @@ public class MainClass06 {
 		 * 모두 다 입력 받은 후 for문을 이용해서 배열에 저장된
 		 * 모든 문자열을 콘솔창에 순서대로 출력하는 코드를 작성해보세요!
 		 */
-		Scanner[] scan = new Scanner[5];
-		for(int i=0; i<scan.length; i++) {
-			Scanner tmp = scan[i];
-			tmp = new Scanner(System.in);
-			System.out.println("문자열을 입력하세요 : "); 
-			String str = tmp.nextLine();
-			//System.out.println(str);
-		}//scan for문 종료
 		
+		String[] str = new String[5];
+		for(int i=0; i<str.length; i++) {
+			Scanner scan = new Scanner (System.in);
+			String tmp = str[i];
+			System.out.println("문자열을 입력해주세요!"); 
+			String a = scan.nextLine();
+			System.out.println(a);
+		}//scan for문 종료
 		
 		/*
 		 *2. Random 객체를 이용해서 로또 번호 6개를 랜덤하게 얻어내서 배열에 저장한다.
 		 *모두 다 저장되면 for 문을 이용해서
 		 *배열에 저장된 모든 로또 번호를 콘솔창에 순서대로 출력하는 코드를 작성해보세요! 
 		 */
-		Random[] ran = new Random[6];
+		int[] ran = new int[6];
 		for(int i=0; i<ran.length; i++) {
-			Random r = ran[i];
-			r = new Random();
-			int lottoNum = r.nextInt(45)+1;
+			int r = ran[i];
+			Random b = new Random();
+			int lottoNum = b.nextInt(45)+1;
 			System.out.println("로또번호는!"+lottoNum);
 		}//Random for문 종료
-		
 		
 	}// void main종료
 }// MainClass06 종료
