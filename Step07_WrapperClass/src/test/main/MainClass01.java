866552c4168196ff2656c5d05a653bde30d1d2c7
liyeon <fldus612@gmail.com>
2020-06-02 12:58:30 +0900

    Step07_wrapperClass 추가

---
+++ Step07_WrapperClass/src/test/main/MainClass01.java
@@ -0,0 +1,26 @@
+package test.main;
+/*
+ * 기본 data type의 참조 data type *
+ * - byte : Byte
+ * - short : Short
+ * - int : Integer
+ * - long : Long
+ * 
+ * - float : Float
+ * - double : Double
+ * 
+ * - char : Character
+ * - boolean : Boolean
+ * 기본데이터타입을조금더편리하게해줄수있는걸wrapperClass라고 한다.
+ * 기본데이터타입에 기능을 가미 해 준다.
+ */
+public class MainClass01 {
+
+	public static void main(String[] args) {
+		//기본데이터 type
+		int num1=10;
+		//참조 데이터 type
+		Integer num2 = 10;
+	}
+
+}
