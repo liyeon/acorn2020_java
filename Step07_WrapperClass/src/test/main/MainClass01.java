5ca25dff6a6351e66aebc192ac3ead50b55ef494
liyeon <fldus612@gmail.com>
2020-06-02 17:40:27 +0900

    Step07_wrapperClass MainClass~02.java추가

--- Step07_WrapperClass/src/test/main/MainClass01.java
+++ Step07_WrapperClass/src/test/main/MainClass01.java
@@ -21,6 +21,20 @@ public class MainClass01 {
 		int num1=10;
 		//참조 데이터 type
 		Integer num2 = 10;
+		
+		//참조데이터 type이지만 기본 데이터 type처럼 사용 할 수 있다.
+		int result1=num2+1;
+		int result2 = num1+num2;
+		Integer result3=num1+num2;
+		
+		//숫자를 문자로 바꿀 때 10-> "10"
+		String strNum = num2.toString();
+		System.out.println(strNum);
+		
+		//문자를 숫자로 바꾸기
+		String strNum2="999";
+		int result4 = Integer.parseInt(strNum); //string을 int로 바꿔줌
+		
 	}
 
 }
