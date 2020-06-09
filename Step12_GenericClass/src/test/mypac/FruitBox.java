6a8f7bc4a7f8de99428ecac1915c01e7d0a12d2d
liyeon <fldus612@gmail.com>
2020-06-09 12:41:51 +0900

    Step12_GenericClass

---
+++ Step12_GenericClass/src/test/mypac/FruitBox.java
@@ -0,0 +1,16 @@
+package test.mypac;
+/*
+ * Class 클래스명<Generic type>
+ * 클래스를 정의 할 때 Generic type을 지정해서 특정 type을 동적으로 지정 할 수 있다.
+ */
+public class FruitBox<T> {//포괄타입을 변수명 적듯이 적는다.
+	//필드
+	private T item;
+	//메소드
+	public void push(T item) {//필드에 전달하는 메소드
+		this.item=item;
+	}
+	public T pull() {//에플타입을 리턴해주는 메소드
+		return item;
+	}
+}
